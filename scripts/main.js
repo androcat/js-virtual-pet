511c141c42de94ba9475be029fa9f8c1e62c4a45
Jay Medina <jay.medina.celis@gmail.com>
2023-05-01 14:35:07 -0400

    Adds complexity to feed button action and added greeting alerts

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 let petName = "Carlos";
-let hungerLevel = 50;
-let mood = "Sad";
+let hungerLevel = 100;
+let mood = "Depressed";
 
 // GEt DOM elements for teh pet's status
+const namePlateEl = document.querySelector(".status-container");
+const octocatEl = document.querySelector("#octocat");
 const petNameEl = document.querySelector("#pet-name");
 const hungerEl = document.querySelector("#hunger-level");
 const moodEl = document.querySelector("#mood");
@@ -13,8 +15,18 @@ const playButton = document.querySelector("#play-button");
 
 // funct declaration vvv
 function feedPet() {
-  if (hungerLevel > 10) {
+  if (hungerLevel >= 90) {
+    hungerLevel -= 5;
+    hungerEl.innerText = hungerLevel + "%";
+    mood = "Depressed";
+    moodEl.innerText = mood;
+  } else if (hungerLevel >= 70) {
     hungerLevel -= 10;
+    hungerEl.innerText = hungerLevel + "%";
+    mood = "?";
+    moodEl.innerText = mood;
+  } else if (hungerLevel > 20) {
+    hungerLevel -= 15;
     hungerEl.innerText = hungerLevel + "%";
     mood = "Sad";
     moodEl.innerText = mood;
@@ -24,11 +36,23 @@ function feedPet() {
     mood = "Happy";
     moodEl.innerText = mood;
   }
+  // console.log(hungerLevel);
 }
+
+function greeting() {
+  if (mood == "Depressed") {
+    return " weeps softly :,^(";
+  } else if (mood == "Sad") {
+    return " lets out a sniffle";
+  } else if (mood == "?") {
+    return " lets out a grunt and says 'grug grug'";
+  } else {
+    return " is finally back to his old self! :^D";
+  }
+}
+
+//alert("Welcome! You just found a sad and abandoned special edition octocat tamagochi that a careless child left behind. Your goal: to make your octocat happy!");
 
 feedButton.addEventListener("click", feedPet);
 
-// funct expression vvv
-// const feedPet = function() {
-//
-// }
+namePlateEl.addEventListener("click", () => alert(petName + greeting()));
