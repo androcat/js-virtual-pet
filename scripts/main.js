66d9f94d1993f5eb1e47715f6e9c1a09d6868f13
Jay Medina <jay.medina.celis@gmail.com>
2023-05-01 14:55:51 -0400

    Adds mood level functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 let petName = "Carlos";
 let hungerLevel = 100;
+let moodLevel = 0;
 let mood = "Depressed";
 
 // GEt DOM elements for teh pet's status
@@ -7,6 +8,7 @@ const namePlateEl = document.querySelector(".status-container");
 const octocatEl = document.querySelector("#octocat");
 const petNameEl = document.querySelector("#pet-name");
 const hungerEl = document.querySelector("#hunger-level");
+const moodLevelEl = document.querySelector("#mood-level");
 const moodEl = document.querySelector("#mood");
 
 // Get DOM elements for teh action buttons
@@ -20,7 +22,8 @@ function feedPet() {
     hungerEl.innerText = hungerLevel + "%";
     mood = "Depressed";
     moodEl.innerText = mood;
-  } else if (hungerLevel >= 70) {
+  } // There is an error here ask Mady bc lost, not acting way i want or expected to
+  else if (hungerLevel >= 70) {
     hungerLevel -= 10;
     hungerEl.innerText = hungerLevel + "%";
     mood = "?";
@@ -33,7 +36,7 @@ function feedPet() {
   } else {
     hungerLevel = 0;
     hungerEl.innerText = 0 + "%";
-    mood = "Happy";
+    mood = "Happy!";
     moodEl.innerText = mood;
   }
   // console.log(hungerLevel);
@@ -51,8 +54,32 @@ function greeting() {
   }
 }
 
+function playPet() {
+  if (moodLevel <= 10) {
+    moodLevel += 5;
+    moodLevelEl.innerText = moodLevel + "%";
+    mood = "Depressed";
+    moodEl.innerText = mood;
+  } else if (moodLevel <= 30) {
+    moodLevel += 15;
+    moodLevelEl.innerText = moodLevel + "%";
+    mood = "?";
+    moodEl.innerText = mood;
+  } else if (moodLevel <= 80) {
+    moodLevel += 20;
+    moodLevelEl.innerText = moodLevel + "%";
+    mood = "Sad";
+    moodEl.innerText = mood;
+  } else {
+    moodLevel = 100;
+    moodLevelEl.innerText = moodLevel + "%";
+    mood = "Elated!";
+    moodEl.innerText = mood;
+  }
+}
+
 //alert("Welcome! You just found a sad and abandoned special edition octocat tamagochi that a careless child left behind. Your goal: to make your octocat happy!");
 
 feedButton.addEventListener("click", feedPet);
-
+playButton.addEventListener("click", playPet);
 namePlateEl.addEventListener("click", () => alert(petName + greeting()));
